0bcfecd518d280a941090302a2fcceaba9877f24
GeffersonDominguez <geffersondominguez93@gmail.com>
2021-08-08 18:32:58 -0500

    Condicional Switch 23-07-2021

---
+++ main.js
@@ -0,0 +1,73 @@
+
+
+// let nombre = prompt(`
+// 1.Nombre completo
+// 2.Primer apellido y segundo apellido
+// 3.Segundo apellido`);
+// var output = 'Salida: ';
+// switch (nombre) {
+//     case `1`:
+//         output += 'Gefferson ';
+//     case `2`:
+//         output += 'Dominguez ';
+//     case `3`:
+//         output += `Galeano`;
+//         alert(output);
+//         break;
+//     default:
+//     alert('Por favor, selecciona un valor del 1 al 3.');
+// }
+
+// let ciudad= prompt(`
+// Ingrese ciudad
+// 1.Bucaramanga
+// 2.Barranquilla
+// 3.Medellin
+// `)
+
+// switch (ciudad) {
+//     case `1`: 
+//     console.log(`Bucaramanga pertenece a Santander`);  
+//         break;
+
+//     case `2`: 
+//     console.log(`Barranquilla pertenece a Antioquia`);  
+//         break;
+
+//     case `3`: 
+//     console.log(`Medellin pertenece a Antioquia`);  
+//         break;
+
+//     default:console.log(`${ciudad} no es un valor permitido`);
+//         break; 
+// }
+
+
+
+// let data = 5;
+// switch (data<=5) {
+//     case true:
+//         console.log(`el numero ${data} es menor o igual 5`);
+//         break;
+//     case false:
+//         console.log(`el numero ${data} no es menor o igual 5`);
+//         break;
+//     default:
+//         console.log(`la opcion ${data} no existe`);
+//         break;
+// }
+
+
+
+
+// switch (data) {
+//     case 5:
+//         console.log(`Estamos en la opcion ${data}`);
+//         break;
+//     case 10:
+//         console.log(`Estamos en la opcion ${data}`);
+//         break;
+//     default:
+//         console.log(`La opcion ${data} no existe`);
+//         break;
+// }
